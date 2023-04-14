895054851291e5eb12d9842218772cdf511b66cd
Bala Mahesh <72618397+Bala534@users.noreply.github.com>
2023-04-14 17:58:29 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,29 @@
+let express = require('express')
+let app = express()
+let mongojs = require('mongojs')
+
+app.get('/', (res,res) => {
+    res.send('welcome to enjoyit')
+})
+
+app.post('/signup', (req,res) => {
+    let data = {
+        firstname: req.body.fname,
+        lastname: req.body.lname,
+        email: req.body.mail,
+        password: req.body.pswd
+    }
+    res.send(data)
+})
+
+app.post('/signin', (req,res) => {
+    let data1 = {
+        email: req.body.mail,
+        password: req.body.pswd
+    }
+    res.send(data1)
+})
+
+app.listen(8000,(req,res)=>{
+    console.log('running')
+})
